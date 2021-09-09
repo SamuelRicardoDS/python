6a9c99a3a0cd29d9a99cc4e0e73e7dea361f32b7
Samuel Ricardo <89036883+SamuelRicardoDS@users.noreply.github.com>
2021-09-09 11:47:13 -0300

    Update while.py

--- while.py
+++ while.py
@@ -1,5 +1,7 @@
 n = 'não'
+ #while loop é um laço condicional, ou seja, necessita de um teste
 while n != 'sim':
+    #neste tipo de laço, enquanto o teste der verdadeiro o comando no bloco de repetição se repete
     n = input('A gente já chegou?')
 
 else:
