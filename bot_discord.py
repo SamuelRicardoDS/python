a22d694ada3ef57ae664b9d04dcd035c1fb6c302
Samuel Ricardo <89036883+SamuelRicardoDS@users.noreply.github.com>
2021-09-26 14:00:53 -0300

    Update bot_discord.py

--- bot_discord.py
+++ bot_discord.py
@@ -1,16 +1,22 @@
 #importando as bibliotecas que fazem o robô de fato exitir
 import discord
 from discord import message
-from discord.ext import commands 
+from discord import channel
+from discord.ext import commands, tasks 
+import datetime #biblioteca para trabalhar com data e hora no python
 
 #comandos em inglês só por boa prática
 
 bot = commands.Bot('!')
 
 #nessa biblioteca as funções funcionam a partir de eventos
+
+#evento de quando o bot funciona
 @bot.event
 async def on_ready(): #quando estiver pronto
     print(f'Estou vivo! Sou eu, {bot.user}')
+
+    current_time.start()
 
 @bot.event #criando as funções que enviam mensagens
 async def on_message(message):
@@ -22,7 +28,7 @@ async def on_message(message):
            f"a luxúria é passageira {message.author.name}, mas a glória é eterna. continue firme até valhalla!"
        )
 
-    if 'scardua' in message.content:
+    if 'engenharia e ambiente' in message.content:
         await message.channel.send(
             f'mantenha-se firme {message.author.name}, este é um inimigo poderoso'
         )
@@ -38,5 +44,15 @@ async def send_hello(ctx):
 
     await ctx.send(answer)
 
+@tasks.loop(hours=24)
+async def current_time(): #definir o que vai acontecer    
+    now = datetime.datetime.now() #pega o horário atual
 
-bot.run('aqui tu coloca o token do teu bot') #rodando de fato o bot
+    now = now.strftime("%d/%m/%Y às %H:%M:%S")   #stringformattime pega dia mes e ano
+
+    channel = bot.get_channel('id do channel que tu quiser que o bot use')
+
+    await channel.send("ESTUDEM!")
+
+
+bot.run('teu token') #rodando de fato o bot
